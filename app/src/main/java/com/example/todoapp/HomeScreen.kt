3c9787044510bf549bf947ecfd9c5285d97552e5
sid19 <sid1992002@gmail.com>
2024-07-22 22:40:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/todoapp/HomeScreen.kt
@@ -0,0 +1,26 @@
+package com.example.todoapp
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun HomeScreen(){
+   Box(modifier = Modifier.fillMaxSize()){
+
+   }
+}
+
+@Preview
+@Composable
+fun Prev(){
+    HomeScreen()
+}
