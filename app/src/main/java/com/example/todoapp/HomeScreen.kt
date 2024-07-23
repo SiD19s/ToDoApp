d4f760401d2a43a318f198500e14e7ff2cbbf093
sid19 <sid1992002@gmail.com>
2024-07-23 20:18:58 +0530

    Done Designing HomeScreen

--- app/src/main/java/com/example/todoapp/HomeScreen.kt
+++ app/src/main/java/com/example/todoapp/HomeScreen.kt
@@ -1,25 +1,100 @@
 package com.example.todoapp
 
+import android.content.res.Configuration
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeScreen(){
    Box(modifier = Modifier.fillMaxSize()){
+       Column(
+           Modifier
+               .align(Alignment.Center)
+               .fillMaxWidth(0.94f)
+               .fillMaxHeight(0.7f),
+           horizontalAlignment = Alignment.CenterHorizontally,
+           verticalArrangement = Arrangement.SpaceBetween
+       ) {
+           Image(
+               painter = painterResource(id = R.drawable.amico)
+               , contentDescription = "",
+               modifier = Modifier
+                   .fillMaxWidth()
+                   .size(400.dp),
+               contentScale = ContentScale.Fit,
+           )
+           Column(
+               modifier = Modifier.fillMaxWidth(),
+           ){
+               Button(
+                   onClick = { /*TODO*/ },
+                   modifier = Modifier.fillMaxWidth(),
+                   shape = RoundedCornerShape(6.dp),
+               ) {
+                   Text(text = "LOGIN")
+               }
+               Button(
+                   onClick = { /*TODO*/ },
+                   modifier = Modifier
+                       .fillMaxWidth()
+                       .padding(top = 12.dp),
+                   shape = RoundedCornerShape(6.dp),
+                   border = BorderStroke(
+                       1.dp,
+                       colorResource(id = R.color.black200)
+                   ),
+                   colors = ButtonColors(
+                       containerColor = Color.White,
+                       disabledContentColor = colorResource(id = R.color.black200),
+                       contentColor = colorResource(id = R.color.black200),
+                       disabledContainerColor = Color.White
+                   )
 
+                   ) {
+                   Text(
+                       text = "SIGN UP",
+                       color = colorResource(id = R.color.black200)
+                   )
+               }
+           }
+       }
    }
 }
 
-@Preview
+
+
+
+@Preview(
+    name = "Light Mode",
+    showBackground = true,
+    uiMode = UI_MODE_NIGHT_NO
+)
 @Composable
 fun Prev(){
     HomeScreen()
